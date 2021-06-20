585cd59bef620faa572b9755857129d6d241f723
NickP2791 <admin@roxanick.com>
2021-06-20 12:55:39 -0500

    rearranged file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './App.css';
+import marked from './marked'
+
+marked.setOptions({
+  breaks: true
+})
+
+const renderer = new marked.Renderer();
 
 function App() {
-  return (
+const [text, setText] = useState('')
+
+const handleChange= (e) => {
+  setText(()=> e.target.value)
+}
+  
+return (
     <div className="App">
-      <h1>Nicolas Pauletto</h1>
+      <h1>My Markdown Previewer</h1>
+      <h2>Markdown editor</h2>
+      <textarea id="editor" name="Editor" rows="10" onChange={handleChange}></textarea>
+      
+      <h2>HTML Output</h2>
+      <div dangerouslySetInnerHTML={{
+        __html: marked({text}, {renderer: renderer}),
+      }} id="preview">
+       <p id="content">Pauletto</p> 
+      </div>
+      {text}
+
     </div>
   );
 }
